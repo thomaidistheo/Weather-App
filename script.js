84b8e8d86f21c2a7d316899db9f1a93a9bf8a023
Thodoris Thomaidis <thodoris.thomaidis@gmail.com>
2021-06-05 21:37:02 +0300

    cleaned up the variables and added error handling for the api call

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-window.addEventListener('load', ()=> {
+window.addEventListener('load', () => {
     let lon
     let lat
     let units = 'metric'
-    const apiKey = 'cb518dc9eec343db4f3ddc77917b8524'
+    const apiKey = config.WEATHER_API
 
-    let date = new Date()
+    let apiErrorMsg = document.querySelector('.location-error')
 
     let currentTime = document.querySelector('.current-time')
     let location = document.querySelector('.location')
@@ -18,45 +18,69 @@ window.addEventListener('load', ()=> {
 
     let visibility
 
-    if(navigator.geolocation) {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-           lon = position.coords.longitude
-           lat = position.coords.latitude
-           console.log(`long: ${lon} lat: ${lat}`)
-           
-           const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=daily&units=${units}&appid=${apiKey}`
-           fetch(api, {
-               mode: 'cors'
-           })
-               .then(res => {
-                   return res.json()
-               })
-               .then(data => {
+            lon = position.coords.longitude
+            lat = position.coords.latitude
+            console.log(`long: ${lon} lat: ${lat}`)
+
+            const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=daily&units=${units}&appid=${apiKey}`
+
+            function handleErrors(res) {
+                if (!res.ok) {
+                    throw Error(res.statusText)
+                }
+                return res
+            }
+
+            fetch(api, {
+                mode: 'cors'
+            })
+                .then(handleErrors)
+                .then(res => {
+                    return res.json()
+                })
+                .then(data => {
                     console.log(data)
 
-                    currentTime.textContent = date /* data.current.dt */
-                    location.textContent = data.timezone
-                    weatherDesc.textContent = data.current.weather[0].description
-                    weatherTemp.textContent = data.current.temp
-                    weatherIcon.src = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`
-                    cloudPercentage.textContent = `${data.current.clouds}%`
-                    humidityPercentage.textContent = `${data.current.humidity}%`
-
-                    visibility = data.current.visibility
-                    
-                    if (visibility > 9999) {
-                        visibility = visibility / 1000
-                        visibilityMetres.textContent = `${visibility}km`
-                    } else if (visibility > 999 && visibility < 9999) {
-                        visibility = visibility / 100
-                        visibilityMetres.textContent = `${visibility}km`
-                    } else {
-                        visibilityMetres.textContent = `${visibility}m`
+                    const currentWeather = {
+                        date: data.current.dt,
+                        timezone: data.timezone,
+                        weatherDesc: data.current.weather[0].description,
+                        weatherTemp: data.current.temp,
+                        weatherIcon: `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`,
+                        cloudPercentage: data.current.clouds,
+                        humidityPercentage: data.current.humidity,
+                        visibility: data.current.visibility
                     }
-               })
+                    populateUI(currentWeather)
+                })
+
         })
     } else {
-        // TODO throw error
+        apiErrorMsg.classList.remove('hidden')
+    }
+
+    let populateUI = (currentWeather) => {
+        currentTime.textContent = currentWeather.date
+        location.textContent = currentWeather.timezone
+        weatherDesc.textContent = currentWeather.weatherDesc
+        weatherTemp.textContent = currentWeather.weatherTemp
+        weatherIcon.src = currentWeather.weatherIcon
+        cloudPercentage.textContent = `${currentWeather.cloudPercentage}%`
+        humidityPercentage.textContent = `${currentWeather.humidityPercentage}%`
+
+        visibility = currentWeather.visibility
+
+        if (visibility > 9999) {
+            visibility = visibility / 1000
+            visibilityMetres.textContent = `${visibility}km`
+        } else if (visibility > 999 && visibility < 9999) {
+            visibility = visibility / 100
+            visibilityMetres.textContent = `${visibility}km`
+        } else {
+            visibilityMetres.textContent = `${visibility}m`
+        }
     }
 })
 
